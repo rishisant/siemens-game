4225836cbf1b5eac2a084ea2560a4b49278b31b9
ethanrutt <ethan.rutt2032@gmail.com>
2024-09-22 18:35:49 -0500

    [REFACTOR] better name and simpler test for sanity check

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -9,19 +9,9 @@ public class NewTestScript
 {
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void NewTestScriptSanityCheck()
     {
         // Use the Assert class to test conditions
         Assert.AreEqual(1, 1, "1 == 1");
-    }
-
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator NewTestScriptWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
     }
 }
