459dddc4c04ed0fb4012dc106690c087ed0783d6
ethanrutt <ethan.rutt2032@gmail.com>
2024-09-22 21:48:38 -0500

    [ADD] basic pipe movement for puzzle game

---
+++ Assets/Scripts/Pipe/PipeMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown() {
+        transform.Rotate(0, 0, 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
