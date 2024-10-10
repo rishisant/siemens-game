c9a9e4e92b502b11942256c9919ad96abca6dfcd
htauL <htauluath@gmail.com>
2024-10-10 12:34:02 -0300

    [ADD] Added a system to change cosmetics

--- Assets/Scripts/Character/Character_Movement.cs
+++ Assets/Scripts/Character/Character_Movement.cs
@@ -1,18 +1,23 @@
 // Rishi Santhanam
 // CSCE 482 Siemens Gamification
 
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
 	[SerializeField] private float charSpeed = 4f;
+	[SerializeField] private CosmeticHandler cosmeticHandler;
 
 	private string currentState;
 
 	private Rigidbody2D rb;
 	private Animator animator;
 	private Animator child_ChestAnimator;
+	private Animator child_LegAnimator;
+	private Animator child_FootAnimator;
+	private Animator child_HeadAnimator;
 
 	private Vector2 movementInputDirection;
 	private Vector2 lastMovementInputDirection;
@@ -26,8 +31,7 @@ public class Character_Movement : MonoBehaviour
 
 
 	private bool syncFlag = false;
-
-
+	private bool isResettingAnimations;
 
 	void Awake()
 	{
@@ -66,6 +70,10 @@ public class Character_Movement : MonoBehaviour
 			{ Vector2.left, "Chest_Idle_Left" },
 			{ Vector2.right, "Chest_Idle_Right" }
 		};
+
+		lastMovementInputDirection = Vector2.down;
+
+		SetChestSprite(cosmeticHandler.GetChestController(0));
 	}
 
 	// Use update for animations
@@ -74,14 +82,23 @@ public class Character_Movement : MonoBehaviour
 		// Update the animator
 		movementInputDirection = GetInput();
 		UpdateAnimator();
-		//Debug.Log("Parent: " + animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
-		//Debug.Log("Child: " + child_ChestAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
 
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			if (child_ChestAnimator.runtimeAnimatorController == cosmeticHandler.GetChestController(0))
+			{
+				SetChestSprite(cosmeticHandler.GetChestController(1));
+			}
+			else
+			{
+				SetChestSprite(cosmeticHandler.GetChestController(0));
+			}
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		HandleMovement();
+		HandleMovement(movementInputDirection);
 	}
 
 	private void ChangePlayerAnimationState(string newState)
@@ -106,13 +123,25 @@ public class Character_Movement : MonoBehaviour
 		return new Vector2(moveHorizontal, moveVertical).normalized;
 	}
 
-	private void HandleMovement()
+	private void HandleMovement(Vector2 movement)
 	{
-		rb.velocity = movementInputDirection * charSpeed;
+		// Movement restriction
+		if (movement.x != 0)
+		{
+			movement.y = 0;
+		}
+		else if (movement.y != 0)
+		{
+			movement.x = 0;
+		}
+
+		rb.velocity = movement * charSpeed;
 	}
 
 	private void UpdateAnimator()
 	{
+		if (isResettingAnimations) return;
+
 		if (movementInputDirection != Vector2.zero)
 		{
 			if (playerDirectionAnimations.ContainsKey(movementInputDirection))
@@ -131,5 +160,79 @@ public class Character_Movement : MonoBehaviour
 				ChangeChestAnimationState(chestIdleAnimations[lastMovementInputDirection]);
 			}
 		}
+	}
+
+	// Set the chest sprite based on the prefab from the CosmeticHandler
+	public void SetChestSprite(RuntimeAnimatorController newController)
+	{
+		if (newController != null)
+		{
+			child_ChestAnimator.runtimeAnimatorController = newController; // Reassign the animator
+
+			SyncAnimations(movementInputDirection);
+		}
+		else
+		{
+			Debug.LogWarning("The provided chest prefab does not have an Animator component.");
+		}
+	}
+
+	private void SyncAnimations(Vector2 movement)
+	{
+		isResettingAnimations = true;
+
+		// Movement restriction
+		if (movement.x != 0)
+		{
+			movement.y = 0;
+
+			if (movement.x > 0)
+			{
+				movement.x = 1;
+			}
+			else if (movement.x < 0)
+			{
+				movement.x = -1;
+			}
+		}
+		else if (movement.y != 0)
+		{
+			movement.x = 0;
+
+			if (movement.y > 0)
+			{
+				movement.y = 1;
+			}
+			else if (movement.y < 0)
+			{
+				movement.y = -1;
+			}
+		}
+
+		if (movement != Vector2.zero)
+		{
+			if (playerDirectionAnimations.ContainsKey(movement))
+			{
+				animator.Play(playerDirectionAnimations[movement], 0, 0f);
+				child_ChestAnimator.Play(chestDirectionAnimations[movement], 0, 0f);
+				lastMovementInputDirection = movement;
+
+				Debug.Log("Playing currentInput: " + chestDirectionAnimations[movement]);
+			}
+
+		}
+		else
+		{
+			if (playerIdleAnimations.ContainsKey(lastMovementInputDirection))
+			{
+				animator.Play(playerIdleAnimations[lastMovementInputDirection], 0, 0f);
+				child_ChestAnimator.Play(chestIdleAnimations[lastMovementInputDirection], 0, 0f);
+			}
+
+			Debug.Log("Playing lastInput: " + chestIdleAnimations[lastMovementInputDirection]);
+		}
+
+
+		isResettingAnimations = false;
 	}
 }
