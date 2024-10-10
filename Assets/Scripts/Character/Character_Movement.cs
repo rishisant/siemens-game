ddcaa3f7893f6f8ea78644a1a697d9f02548ce9c
htauL <htauluath@gmail.com>
2024-10-09 23:28:32 -0300

    [FIX] Solved the animation movement issue

--- Assets/Scripts/Character/Character_Movement.cs
+++ Assets/Scripts/Character/Character_Movement.cs
@@ -1,78 +1,135 @@
 // Rishi Santhanam
 // CSCE 482 Siemens Gamification
 
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
-    [SerializeField] private float charSpeed = 4f;
+	[SerializeField] private float charSpeed = 4f;
 
-    private bool isMoving = false;
-    private Vector2 lastMovement = Vector2.down;
-    
-    private Rigidbody2D rb;
-    private Animator animator;
-    private Animator child_ChestAnimator;
+	private string currentState;
 
-    private bool syncFlag = false;
+	private Rigidbody2D rb;
+	private Animator animator;
+	private Animator child_ChestAnimator;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        child_ChestAnimator = transform.GetChild(0).GetComponent<Animator>();
-    }
+	private Vector2 movementInputDirection;
+	private Vector2 lastMovementInputDirection;
 
-    // Use update for animations
-    private void Update()
-    {
-        // Update the animator
-        Vector2 movementInput = GetInput();
-        UpdateAnimator(movementInput);
-        HandleMovement(movementInput);
-        Debug.Log("Parent: " + animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
-        Debug.Log("Child: " + child_ChestAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
 
-    }
+	private Dictionary<Vector2, string> playerDirectionAnimations;
+	private Dictionary<Vector2, string> playerIdleAnimations;
 
-    private Vector2 GetInput()
-    {
-        float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        return new Vector2(moveHorizontal, moveVertical).normalized;
-    }
+	private Dictionary<Vector2, string> chestDirectionAnimations;
+	private Dictionary<Vector2, string> chestIdleAnimations;
 
-    private void HandleMovement(Vector2 movement)
-    {
-        if (movement.x != 0)
-        {
-            movement.y = 0;
-        }
-        else if (movement.y != 0)
-        {
-            movement.x = 0;
-        }
 
-        rb.velocity = movement * charSpeed; // Use charSpeed
-        isMoving = movement != Vector2.zero;
-    }
+	private bool syncFlag = false;
 
-    private void UpdateAnimator(Vector2 movement)
-    {
-        animator.SetFloat("MovingX", movement.x);
-        animator.SetFloat("MovingY", movement.y);
-        animator.SetBool("IsMoving", isMoving);
 
-        // Update the child animator (Chest)
-        child_ChestAnimator.SetFloat("MovingX", movement.x);
-        child_ChestAnimator.SetFloat("MovingY", movement.y);
-        child_ChestAnimator.SetBool("IsMoving", isMoving);
 
-        if (isMoving)
-        {
-            lastMovement = movement;
-        }
-    }
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		child_ChestAnimator = transform.GetChild(0).GetComponent<Animator>();
+
+		playerDirectionAnimations = new Dictionary<Vector2, string>
+		{
+			{ Vector2.down, "Char_Walk_Down" },
+			{ Vector2.up, "Char_Walk_Up" },
+			{ Vector2.left, "Char_Walk_Left" },
+			{ Vector2.right, "Char_Walk_Right" }
+		};
+
+		playerIdleAnimations = new Dictionary<Vector2, string>
+		{
+			{ Vector2.down, "Char_Idle_Down" },
+			{ Vector2.up, "Char_Idle_Up" },
+			{ Vector2.left, "Char_Idle_Left" },
+			{ Vector2.right, "Char_Idle_Right" }
+		};
+
+		chestDirectionAnimations = new Dictionary<Vector2, string>
+		{
+			{ Vector2.down, "Chest_Walk_Down" },
+			{ Vector2.up, "Chest_Walk_Up" },
+			{ Vector2.left, "Chest_Walk_Left" },
+			{ Vector2.right, "Chest_Walk_Right" }
+		};
+
+		chestIdleAnimations = new Dictionary<Vector2, string>
+		{
+			{ Vector2.down, "Chest_Idle_Down" },
+			{ Vector2.up, "Chest_Idle_Up" },
+			{ Vector2.left, "Chest_Idle_Left" },
+			{ Vector2.right, "Chest_Idle_Right" }
+		};
+	}
+
+	// Use update for animations
+	private void Update()
+	{
+		// Update the animator
+		movementInputDirection = GetInput();
+		UpdateAnimator();
+		//Debug.Log("Parent: " + animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+		//Debug.Log("Child: " + child_ChestAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+	}
+
+	private void FixedUpdate()
+	{
+		HandleMovement();
+	}
+
+	private void ChangePlayerAnimationState(string newState)
+	{
+		if (currentState == newState) return;
+
+		animator.Play(newState);
+		currentState = newState;
+	}
+	private void ChangeChestAnimationState(string newState)
+	{
+		if (currentState == newState) return;
+
+		child_ChestAnimator.Play(newState);
+		currentState = newState;
+	}
+
+	private Vector2 GetInput()
+	{
+		float moveHorizontal = Input.GetAxisRaw("Horizontal");
+		float moveVertical = Input.GetAxisRaw("Vertical");
+		return new Vector2(moveHorizontal, moveVertical).normalized;
+	}
+
+	private void HandleMovement()
+	{
+		rb.velocity = movementInputDirection * charSpeed;
+	}
+
+	private void UpdateAnimator()
+	{
+		if (movementInputDirection != Vector2.zero)
+		{
+			if (playerDirectionAnimations.ContainsKey(movementInputDirection))
+			{
+				ChangePlayerAnimationState(playerDirectionAnimations[movementInputDirection]);
+				ChangeChestAnimationState(chestDirectionAnimations[movementInputDirection]);
+				lastMovementInputDirection = movementInputDirection;
+			}
+
+		}
+		else
+		{
+			if (playerIdleAnimations.ContainsKey(lastMovementInputDirection))
+			{
+				ChangePlayerAnimationState(playerIdleAnimations[lastMovementInputDirection]);
+				ChangeChestAnimationState(chestIdleAnimations[lastMovementInputDirection]);
+			}
+		}
+	}
 }
