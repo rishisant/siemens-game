ca5141824c185e64bcbefa8b0d7a8e32270f5918
ethanrutt <ethan.rutt2032@gmail.com>
2024-10-13 20:58:57 -0500

    [ADD] different levels for wire game

--- Assets/Scripts/Wires/PoweredWireBehavior.cs
+++ Assets/Scripts/Wires/PoweredWireBehavior.cs
@@ -3,30 +3,60 @@ using System.Collections.Generic;
 using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
 
+/**
+ * Behavior logic for a poweredWire
+ * 
+ * Makes heavy use of the PoweredWireStats class to keep track of variables
+ * @see PoweredWireStats
+ */
 public class PoweredWireBehavior : MonoBehaviour
 {
+    // bool for if the mouse is down
     bool mouseDown = false;
+
+    // statistics for this individual wire
     public PoweredWireStats powerWireS;
+
+    // the line renderer that makes these sprites work together to look like a wire
     LineRenderer line;
 
-    // Start is called before the first frame update
+    /**
+     * Start() is a Unity function that is called before the first frame update
+     * 
+     * This just grabs the appropriate members using the GetComponent() Unity function
+     * both powerWireStats and the LineRenderer are grabbed
+     */
     void Start()
     {
         powerWireS = gameObject.GetComponent<PoweredWireStats>();
         line = gameObject.GetComponentInParent<LineRenderer>();
     }
 
-    // Update is called once per frame
+    /**
+     * Update() is a Unity function that is called every frame update
+     *
+     * This calls the MoveWire() function
+     */
     void Update()
     {
         MoveWire();
     }
 
+    /**
+     * OnMouseDown() is a Unity function that runs whenever the mouse is down
+     * 
+     * This just sets the mouseDown variable to true
+     */
     void OnMouseDown()
     {
         mouseDown = true;
     }
 
+    /**
+     * OnMouseOver() is a Unity function that runs whenever the mouse is over this gameObject
+     * 
+     * This sets the wire to be movable if it's not already connected
+     */
     void OnMouseOver()
     {
         if (powerWireS.connected)
@@ -37,6 +67,11 @@ public class PoweredWireBehavior : MonoBehaviour
         powerWireS.movable = true;
     }
 
+    /**
+     * OnMouseExit() is a Unity function that runs whenever the mouse stops hovering over the GUI element
+     * 
+     * This sets the wire to no longer be movable if it's not currently moving
+     */
     void OnMouseExit()
     {
         if (powerWireS.connected)
@@ -50,6 +85,11 @@ public class PoweredWireBehavior : MonoBehaviour
         }
     }
 
+    /**
+     * OnMouseUp() is a Unity function that runs whenever the user has released the mouse button
+     * 
+     * This resets the wire to its start position and calls the UpdateLine() function
+     */
     void OnMouseUp()
     {
         if (powerWireS.connected)
@@ -62,6 +102,11 @@ public class PoweredWireBehavior : MonoBehaviour
         UpdateLine();
     }
 
+    /**
+     * MoveWire() handles the logic for moving the wire
+     * 
+     * The wire will follow the position of the Input, which is normally the mouse or the finger
+     */
     void MoveWire()
     {
         if (powerWireS.connected)
@@ -86,6 +131,10 @@ public class PoweredWireBehavior : MonoBehaviour
         }
     }
 
+    /**
+     * UpdateLine() will handle the LineRenderer updates that need to happen
+     * The LineRenderer trails behind the front object (wireLength) by 0.1 units
+     */
     public void UpdateLine()
     {
         line.SetPosition(1, new Vector3(gameObject.transform.position.x - .1f, gameObject.transform.position.y, 0));
--- Assets/Scripts/Wires/PoweredWireStats.cs
+++ Assets/Scripts/Wires/PoweredWireStats.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/**
+ * Statistics class for powered wires. This is mainly a dataclass that
+ * keeps track of the wires start position and other helpful flags for an
+ * individual wire
+ * 
+ * All logic for moving the wire is in PoweredWireBehavior
+ * @see PoweredWireBehavior
+ */
 public class PoweredWireStats : MonoBehaviour
 {
+    // bool for if the wire is able to be moved. mainly for checking that it's not
+    // already being acted upon by a mouse or finger, or already connected
     public bool movable = false;
+
+    // bool for if the wire is currently moving
     public bool moving = false;
+
+    // bool for if the wire is connected to the correct wirePlug
     public bool connected = false;
+
+    // the initial position of the wireLength
     public Vector3 startPosition;
 
-    // Start is called before the first frame update
+    /**
+     * Start() is a Unity function that is called before the first frame
+     * update. All that happens inside this is that the startPosition member is
+     * set to the current transform.position
+     */
     void Start()
     {
         startPosition = transform.position;
