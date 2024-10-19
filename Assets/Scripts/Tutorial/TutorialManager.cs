ede3426bd4d0b30e9fa2927dd896c54ff51204fc
Rishi Santhanam <rishisant@tamu.edu>
2024-10-18 21:51:31 -0500

    [ADD] Tutorial dialogue manager and flags

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Essentially, want to have a tutorial manager that allows for Sensei to give the
+// player instructions through dialogue. This will be done through a dialogue manager
+// that will be called by the tutorial manager. The tutorial manager will be responsible
+public class TutorialManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
